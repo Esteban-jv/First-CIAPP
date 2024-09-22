70ebe4c46fdbab75b88fde1adee645de9531c369
Esteban-jv <ejv.developer@gmail.com>
2024-09-22 13:08:59 -0500

    Starting with Email configuration

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,22 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        /*return view('header', ['title' => 'Home']) // this way is more common
-            .view('Home/index');*/
+        $this->sendTestEmail();
         return view('Home/index');
+    }
+
+    private function sendTestEmail(): void
+    {
+        $email = \Config\Services::email();
+//        $email->setFrom('ejv.developer@gmail.com');
+        $email->setTo('mailexample@example.com');
+        $email->setSubject('Test email');
+        $email->setMessage("This is a test email from <b>CodeIgniter 4</b>.");
+
+        if($email->send()){
+            echo "Email sent";
+        }else{
+            echo 'Email not sent';
+        }
     }
 }
--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -4,4 +4,14 @@
 
 <?= $this->section('content') ?>
     <h1>Welcome to  custom CodeIgniter 4!</h1>
+
+    <?php if (auth()->loggedIn()): ?>
+        <p>Hello, <?= auth()->user()->name ?></p>
+        <br>
+        <a href="<?= url_to('Articles::index') ?>">Manage articles</a>
+        <br><br>
+        <a href="<?= url_to('logout') ?>">Logout</a>
+    <?php else: ?>
+        <a href="<?= url_to('login') ?>">Login</a>
+    <?php endif; ?>
 <?= $this->endSection() ?>
